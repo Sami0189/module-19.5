bdcd382d70b26c50799904ef41aa08df3c9cb358
Sami0189 <samisaimon28@gmail.com>
2024-10-28 13:52:06 +0600

    reverse

---
+++ reverse.js
@@ -0,0 +1,32 @@
+//reverse
+const numbers = [1,2,3,4,5,6,7,8];
+//console.log(numbers);
+
+//numbers.reverse();
+//console.log(numbers);
+
+
+
+// const revNumbers = [];
+
+// for (const num of numbers){  
+//     console.log(num)
+//     revNumbers.unshift(num)
+// }
+// console.log(revNumbers)
+
+
+// for(let i =0;i<numbers.length;i++){
+//     const num = numbers[i];
+//     revNumbers.unshift(num)
+// }
+
+
+
+
+//reverse side
+
+for(let i= numbers.length -1; i>=0; i--){
+    const num = numbers[i];
+    console.log(num);
+}
